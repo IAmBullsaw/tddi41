c630f49fa554134c8f949144f5df03bd41d5fd95
Niklas Nson <niklasnson@me.com>
2017-11-15 06:34:51 +0100

    final tests for ntp, missing something?

--- tests/test_ntp.rb
+++ tests/test_ntp.rb
@@ -2,14 +2,14 @@ class NTPTests
 
   def initialize()
     @hostname = %x(hostname)
-    @expected_source = "ida-gw" if @hostname == "gw"
-    @expected_source = "gw" if @hostname == "gw"
+    @expected_source = "*ida-gw" if @hostname == "gw"
+    @expected_source = "*gw" if @hostname == "gw"
   end
 
   def run
     result = %(ntpq -p)
-    source = result
-    reach = result
+    source = result.split("\n")[-1].split[0].split('.')[0]
+    assert_equal(source, @expected_source)
   end
 
   def assert_equal(a,b)
