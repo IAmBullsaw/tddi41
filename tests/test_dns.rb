cfe15008ad6a306915168ead8b2521629e10e8a3
Niklas Nson <niklasnson@me.com>
2017-11-16 06:06:52 +0100

    added final tests for dns

--- tests/test_dns.rb
+++ tests/test_dns.rb
@@ -5,7 +5,7 @@ class DNSTest
      @domain = domain
      @ip = nil
      @hostname = nil
-     @interface = "en0"
+     @interface = "eth0"
 
      setup
     end
@@ -16,10 +16,13 @@ class DNSTest
     end
 
     def run
+      puts "All tests should respond with TRUE"
+      puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
       test_name_resolution
       test_reverse_dns
+      test_dig_hosts
+      test_ping_hosts
     end
-
 
     def test_name_resolution
       @hosts.each do |hostname, ip|
@@ -34,6 +37,18 @@ class DNSTest
         next if @hostname.to_s == hostname.to_s
         host_name = %x(dig +short -x #{ip})
         puts assert_equal(host_name, hostname)
+      end
+    end
+
+    def test_dig_hosts
+      @hosts.each do |key, value|
+        puts %x(dig #{key}.51.sysinst.ida.liu.se).split.include?("sysinst.ida.liu.se.")
+      end
+    end
+
+    def test_ping_hosts
+      @hosts.each do |key, value|
+        puts %x(dig #{value}.51.sysinst.ida.liu.se).split.include?("sysinst.ida.liu.se.")
       end
     end
 
