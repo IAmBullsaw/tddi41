ec8e699a48191aba936eb96046c597a75ae51592
Oskar Jansson <oskja067@student.liu.se>
2017-12-04 12:22:40 +0100

    fixes: test_networking.rb

--- tests/test_networking.rb
+++ tests/test_networking.rb
@@ -2,8 +2,8 @@ class NetworkingTest
   def initialize(hostnames, addresses)
     @hostnames = hostnames
     @addresses = addresses
-    @ip = %x(hostname --ip-address)
-    @hostname = %x(hostname)
+    @ip = %x(hostname --ip-address).strip!
+    @hostname = %x(hostname).strip!
   end
 
   def run
@@ -17,33 +17,37 @@ class NetworkingTest
   end
 
   def test_basic_networking
-    addresses.each do |addr|
+    puts "-> test_basic_networking"
+    @addresses.each do |addr|
       next if @ip.to_s == addr.to_s
-      res = %x(ping -c 1 #{addr} >/dev/null && echo 'OK')
+      res = %x(ping -c 1 #{addr} >/dev/null && echo 'OK').strip!
       puts assert_equal("OK", res)
     end
   end
 
   def test_hostname
+    puts "-> test_hostname"
     puts @hostnames.include? @hostname
   end
 
   def test_ip
+    puts "-> test_ip"
     puts @addresses.include? @ip
   end
 
   def test_ping_on_ip
+    puts "-> test_ping_on_ip"
     for addr in @addresses do
-      puts assert_equal(%x(ping -c 1 #{addr} | grep "1 packets").split[6], "0.0%")
+      puts assert_equal(%x(ping -c 1 #{addr} | grep "1 packets").split[5].to_i, 0)
     end
   end
 
   def test_ping_on_hostname
+    puts "-> test_ping_on_hostname"
     for name in @hostnames do
-      puts assert_equal(%x(ping -c 1 #{name} | grep "1 packets").split[6], "0.0%")
+      puts assert_equal(%x(ping -c 1 #{name} | grep "1 packets").split[5].to_i, 0)
     end
   end
-
 
   def assert_equal(a,b)
     return a == b
